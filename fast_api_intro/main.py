b86cefe8e8693d913419f830766be90f8374d9fc
johnloane <john.loane@dkit.ie>
2026-09-23 15:59:35 +0100

    Add Validation and Error Handling
    Homework: Fix the /readings/{reading_id} end point to display the
    reading.html page rather than returning JSON

--- fast_api_intro/main.py
+++ fast_api_intro/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 sensor_readings: list[dict] = [
     {
@@ -20,8 +21,10 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", include_in_schema=False)
-@app.get("/temp_sensor_readings", include_in_schema=False)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/temp_sensor_readings", include_in_schema=False, name = "temp_sensor_readings")
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"sensor_readings":sensor_readings, "title":"IoT sensor readings"})
 
@@ -29,3 +32,19 @@ def home(request: Request):
 @app.get("/api/sensor_readings")
 def get_sensor_readings():
     return sensor_readings
+
+
+@app.get("/api/readings/{reading_id}")
+def get_reading(request, reading_id: int):
+    for reading in sensor_readings:
+        if reading.get("id") == reading_id:
+            return templates.TemplateResponse(request, "reading.html", {"reading":reading, "title":"IoT sensor readings"})
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="Sensor reading not found")
+
+
+@app.get("/readings/{reading_id}")
+def reading_page(reading_id: int):
+    for reading in sensor_readings:
+        if reading.get("id") == reading_id:
+            return reading
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="Sensor reading not found")
